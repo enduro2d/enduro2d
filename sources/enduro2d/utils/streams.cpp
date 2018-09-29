d5b3542f6e729ece172c03472d47725f484e03ee
BlackMATov <blackmatov@gmail.com>
2018-09-29 23:53:48 +0700

    string operations for streams

--- sources/enduro2d/utils/streams.cpp
+++ sources/enduro2d/utils/streams.cpp
@@ -65,6 +65,129 @@ namespace
 
 namespace e2d
 {
+    //
+    // input_sequence
+    //
+
+    input_sequence::input_sequence(input_stream& stream) noexcept
+    : stream_(stream) {}
+
+    bool input_sequence::success() const noexcept {
+        return success_;
+    }
+
+    std::exception_ptr input_sequence::exception() const noexcept {
+        return exception_;
+    }
+
+    input_sequence& input_sequence::seek(std::ptrdiff_t offset, bool relative) noexcept {
+        try {
+            if ( success_ ) {
+                stream_.seek(offset, relative);
+            }
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    input_sequence& input_sequence::read(void* dst, std::size_t size) noexcept {
+        try {
+            success_ = success_ && stream_.read(dst, size) == size;
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    input_sequence& input_sequence::read_all(str& dst) noexcept {
+        try {
+            if ( success_ ) {
+                vector<char> tail(stream_.length() - stream_.tell());
+                if ( tail.size() == stream_.read(tail.data(), tail.size()) ) {
+                    dst.assign(tail.data(), tail.data() + tail.size());
+                } else {
+                    success_ = false;
+                }
+            }
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    input_sequence& input_sequence::read_all(buffer& dst) noexcept {
+        try {
+            if ( success_ ) {
+                buffer tail(stream_.length() - stream_.tell());
+                if ( tail.size() == stream_.read(tail.data(), tail.size()) ) {
+                    tail.swap(dst);
+                } else {
+                    success_ = false;
+                }
+            }
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    //
+    // output_sequence
+    //
+
+    output_sequence::output_sequence(output_stream& stream) noexcept
+    : stream_(stream) {}
+
+    bool output_sequence::success() const noexcept {
+        return success_;
+    }
+
+    std::exception_ptr output_sequence::exception() const noexcept {
+        return exception_;
+    }
+
+    output_sequence& output_sequence::seek(std::ptrdiff_t offset, bool relative) noexcept {
+        try {
+            if ( success_ ) {
+                stream_.seek(offset, relative);
+            }
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    output_sequence& output_sequence::write(const void* src, std::size_t size) noexcept {
+        try {
+            success_ = success_ && stream_.write(src, size) == size;
+        } catch (...) {
+            success_ = false;
+            exception_ = std::current_exception();
+        }
+        return *this;
+    }
+
+    output_sequence& output_sequence::write_all(const buffer& src) noexcept {
+        return success_
+            ? write(src.data(), src.size())
+            : *this;
+    }
+
+    output_sequence& output_sequence::write_all(const str& src) noexcept {
+        return success_
+            ? write(src.c_str(), src.size())
+            : *this;
+    }
+}
+
+namespace e2d
+{
     input_stream_uptr make_memory_stream(buffer data) noexcept {
         try {
             return std::make_unique<memory_stream>(std::move(data));
@@ -76,28 +199,35 @@ namespace e2d
 
 namespace e2d { namespace streams
 {
+    bool try_read_tail(str& dst, const input_stream_uptr& stream) noexcept {
+        return stream
+            ? input_sequence(*stream)
+                .read_all(dst)
+                .success()
+            : false;
+    }
+
     bool try_read_tail(buffer& dst, const input_stream_uptr& stream) noexcept {
-        try {
-            if ( stream ) {
-                buffer tail(stream->length() - stream->tell());
-                if ( tail.size() == stream->read(tail.data(), tail.size()) ) {
-                    dst.swap(tail);
-                    return true;
-                }
-            }
-        } catch (...) {
-            // nothing
-        }
-        return false;
+        return stream
+            ? input_sequence(*stream)
+                .read_all(dst)
+                .success()
+            : false;
+    }
+
+    bool try_write_tail(const str& src, const output_stream_uptr& stream) noexcept {
+        return stream
+            ? output_sequence(*stream)
+                .write_all(src)
+                .success()
+            : false;
     }
 
     bool try_write_tail(const buffer& src, const output_stream_uptr& stream) noexcept {
-        try {
-            return
-                stream &&
-                src.size() == stream->write(src.data(), src.size());
-        } catch (...) {
-            return false;
-        }
+        return stream
+            ? output_sequence(*stream)
+                .write_all(src)
+                .success()
+            : false;
     }
 }}
