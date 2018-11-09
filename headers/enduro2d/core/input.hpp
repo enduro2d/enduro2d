f0d4dd9bad933fb3ac66ffee1252f7b17e7448c2
BlackMATov <blackmatov@gmail.com>
2018-11-09 16:36:03 +0700

    Fix issue: https://github.com/enduro2d/enduro2d/issues/22

--- headers/enduro2d/core/input.hpp
+++ headers/enduro2d/core/input.hpp
@@ -27,13 +27,13 @@ namespace e2d
         bool is_button_just_pressed(mouse_button btn) const noexcept;
         bool is_button_just_released(mouse_button btn) const noexcept;
 
-        void extract_pressed_buttons(std::vector<mouse_button>& dst) const;
-        void extract_just_pressed_buttons(std::vector<mouse_button>& dst) const;
-        void extract_just_released_buttons(std::vector<mouse_button>& dst) const;
+        void extract_pressed_buttons(vector<mouse_button>& dst) const;
+        void extract_just_pressed_buttons(vector<mouse_button>& dst) const;
+        void extract_just_released_buttons(vector<mouse_button>& dst) const;
     private:
-        class state;
         friend class input;
-        std::unique_ptr<state> state_;
+        class internal_state;
+        std::unique_ptr<internal_state> state_;
     };
 
     class keyboard final : private noncopyable {
@@ -52,50 +52,50 @@ namespace e2d
         bool is_key_just_pressed(keyboard_key key) const noexcept;
         bool is_key_just_released(keyboard_key key) const noexcept;
 
-        void extract_pressed_keys(std::vector<keyboard_key>& dst) const;
-        void extract_just_pressed_keys(std::vector<keyboard_key>& dst) const;
-        void extract_just_released_keys(std::vector<keyboard_key>& dst) const;
+        void extract_pressed_keys(vector<keyboard_key>& dst) const;
+        void extract_just_pressed_keys(vector<keyboard_key>& dst) const;
+        void extract_just_released_keys(vector<keyboard_key>& dst) const;
     private:
-        class state;
         friend class input;
-        std::unique_ptr<state> state_;
+        class internal_state;
+        std::unique_ptr<internal_state> state_;
     };
 
     class input final : public module<input> {
     public:
         struct input_char_event {
-            char32_t uchar = 0;
-            input_char_event(char32_t uchar)
-            : uchar(uchar) {}
+            char32_t uchar{0};
+            input_char_event(char32_t nuchar)
+            : uchar(nuchar) {}
         };
         struct move_cursor_event {
-            v2f pos = v2f::zero();
-            move_cursor_event(const v2f& pos)
-            : pos(pos) {}
+            v2f pos{v2f::zero()};
+            move_cursor_event(const v2f& npos)
+            : pos(npos) {}
         };
         struct mouse_scroll_event {
-            v2f delta = v2f::zero();
-            mouse_scroll_event(const v2f& delta)
-            : delta(delta) {}
+            v2f delta{v2f::zero()};
+            mouse_scroll_event(const v2f& ndelta)
+            : delta(ndelta) {}
         };
         struct mouse_button_event {
-            mouse_button button = mouse_button::unknown;
-            mouse_button_action action = mouse_button_action::unknown;
-            mouse_button_event(mouse_button btn, mouse_button_action act)
-            : button(btn)
-            , action(act) {}
+            mouse_button button{mouse_button::unknown};
+            mouse_button_action action{mouse_button_action::unknown};
+            mouse_button_event(mouse_button nbutton, mouse_button_action naction)
+            : button(nbutton)
+            , action(naction) {}
 
         };
         struct keyboard_key_event {
-            keyboard_key key = keyboard_key::unknown;
-            keyboard_key_action action = keyboard_key_action::unknown;
-            keyboard_key_event(keyboard_key key, keyboard_key_action act)
-            : key(key)
-            , action(act) {}
+            keyboard_key key{keyboard_key::unknown};
+            keyboard_key_action action{keyboard_key_action::unknown};
+            keyboard_key_event(keyboard_key nkey, keyboard_key_action naction)
+            : key(nkey)
+            , action(naction) {}
         };
     public:
         input();
-        ~input() noexcept;
+        ~input() noexcept final;
 
         const class mouse& mouse() const noexcept;
         const class keyboard& keyboard() const noexcept;
@@ -108,8 +108,8 @@ namespace e2d
 
         void frame_tick() noexcept;
     private:
-        class state;
-        std::unique_ptr<state> state_;
+        class internal_state;
+        std::unique_ptr<internal_state> state_;
     };
 
     class window_input_source : public window::event_listener {
