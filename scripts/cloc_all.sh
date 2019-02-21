e75f819789fbcf01e9d4932d37a3cf5f868181c9
BlackMATov <blackmatov@gmail.com>
2019-02-21 15:30:10 +0700

    remove toolset to https://github.com/enduro2d/enduro2d-toolset

--- scripts/cloc_all.sh
+++ scripts/cloc_all.sh
@@ -4,5 +4,4 @@ cloc \
   $SCRIPT_DIR/../headers/enduro2d \
   $SCRIPT_DIR/../sources/enduro2d \
   $SCRIPT_DIR/../samples/sources \
-  $SCRIPT_DIR/../untests/sources \
-  $SCRIPT_DIR/../toolset/model_converter/sources
+  $SCRIPT_DIR/../untests/sources
