99a2c6999361900e3d26149157e6474ad87419cd
BlackMATov <blackmatov@gmail.com>
2019-03-05 01:55:35 +0700

    tint and properties for sprite_renderer

--- headers/enduro2d/high/components/sprite_renderer.hpp
+++ headers/enduro2d/high/components/sprite_renderer.hpp
@@ -17,10 +17,18 @@ namespace e2d
         sprite_renderer() = default;
         sprite_renderer(const sprite_asset::ptr& sprite);
 
+        sprite_renderer& tint(const color& value) noexcept;
+        const color& tint() const noexcept;
+
         sprite_renderer& sprite(const sprite_asset::ptr& value) noexcept;
         const sprite_asset::ptr& sprite() const noexcept;
+
+        render::property_block& properties() noexcept;
+        const render::property_block& properties() const noexcept;
     private:
+        color tint_;
         sprite_asset::ptr sprite_;
+        render::property_block properties_;
     };
 }
 
@@ -29,6 +37,15 @@ namespace e2d
     inline sprite_renderer::sprite_renderer(const sprite_asset::ptr& sprite)
     : sprite_(sprite) {}
 
+    inline sprite_renderer& sprite_renderer::tint(const color& value) noexcept {
+        tint_ = value;
+        return *this;
+    }
+
+    inline const color& sprite_renderer::tint() const noexcept {
+        return tint_;
+    }
+
     inline sprite_renderer& sprite_renderer::sprite(const sprite_asset::ptr& value) noexcept {
         sprite_ = value;
         return *this;
@@ -36,5 +53,13 @@ namespace e2d
 
     inline const sprite_asset::ptr& sprite_renderer::sprite() const noexcept {
         return sprite_;
+    }
+
+    inline render::property_block& sprite_renderer::properties() noexcept {
+        return properties_;
+    }
+
+    inline const render::property_block& sprite_renderer::properties() const noexcept {
+        return properties_;
     }
 }
