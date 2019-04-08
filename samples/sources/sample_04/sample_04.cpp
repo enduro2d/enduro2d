90d1ae292122571e90512405ba652e039f140480
BlackMATov <blackmatov@gmail.com>
2019-04-08 13:44:30 +0700

    dummy sample 04

---
+++ samples/sources/sample_04/sample_04.cpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * This file is part of the "Enduro2D"
+ * For conditions of distribution and use, see copyright notice in LICENSE.md
+ * Copyright (C) 2018-2019, by Matvey Cherevko (blackmatov@gmail.com)
+ ******************************************************************************/
+
+#include "../common.hpp"
+using namespace e2d;
+
+namespace
+{
+    class game_system final : public ecs::system {
+    public:
+        void process(ecs::registry& owner) override {
+            E2D_UNUSED(owner);
+            const keyboard& k = the<input>().keyboard();
+
+            if ( k.is_key_just_released(keyboard_key::f12) ) {
+                the<dbgui>().toggle_visible(!the<dbgui>().visible());
+            }
+
+            if ( k.is_key_just_released(keyboard_key::escape) ) {
+                the<window>().set_should_close(true);
+            }
+
+            if ( k.is_key_pressed(keyboard_key::lsuper) && k.is_key_just_released(keyboard_key::enter) ) {
+                the<window>().toggle_fullscreen(!the<window>().fullscreen());
+            }
+        }
+    };
+
+    class camera_system final : public ecs::system {
+    public:
+        void process(ecs::registry& owner) override {
+            owner.for_joined_components<camera>(
+            [](const ecs::const_entity&, camera& cam){
+                if ( !cam.target() ) {
+                    cam.viewport(
+                        the<window>().real_size());
+                    cam.projection(math::make_orthogonal_lh_matrix4(
+                        the<window>().real_size().cast_to<f32>(), 0.f, 1000.f));
+                }
+            });
+        }
+    };
+
+    class game final : public high_application {
+    public:
+        bool initialize() final {
+            return create_camera()
+                && create_systems();
+        }
+    private:
+        bool create_camera() {
+            ecs::entity camera_e = the<world>().registry().create_entity();
+            ecs::entity_filler(camera_e)
+                .component<camera>(camera()
+                    .background({1.f, 0.4f, 0.f, 1.f}))
+                .component<actor>(node::create(camera_e));
+            return true;
+        }
+
+        bool create_systems() {
+            ecs::registry_filler(the<world>().registry())
+                .system<game_system>(world::priority_update)
+                .system<camera_system>(world::priority_pre_render);
+            return true;
+        }
+    };
+}
+
+int e2d_main(int argc, char *argv[]) {
+    const auto starter_params = starter::parameters(
+        engine::parameters("sample_04", "enduro2d")
+            .timer_params(engine::timer_parameters()
+                .maximal_framerate(100)));
+    modules::initialize<starter>(argc, argv, starter_params).start<game>();
+    modules::shutdown<starter>();
+    return 0;
+}
