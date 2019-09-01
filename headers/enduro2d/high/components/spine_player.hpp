cf04cebf441ab14999b99a929c83701ba748fdc5
BlackMATov <blackmatov@gmail.com>
2019-09-02 02:52:31 +0700

    refactoring of spine_player loading

--- headers/enduro2d/high/components/spine_player.hpp
+++ headers/enduro2d/high/components/spine_player.hpp
@@ -11,53 +11,45 @@
 #include "../factory.hpp"
 #include "../assets/spine_model_asset.hpp"
 
-struct spAnimationState;
 struct spSkeleton;
 struct spSkeletonClipping;
-struct spVertexEffect;
-struct spTrackEntry;
-struct spEvent;
+struct spAnimationState;
 
 namespace e2d
 {
+    class bad_spine_player_access final : public exception {
+    public:
+        const char* what() const noexcept final {
+            return "bad spine player access";
+        }
+    };
+
     class spine_player final {
     public:
-        class on_complete_event final {
-        public:
-            on_complete_event() = default;
-
-            std::vector<str>& completed() noexcept { return completed_; }
-            const std::vector<str>& completed() const noexcept { return completed_; }
-        private:
-            std::vector<str> completed_;
-        };
-
-        using animation_ptr = std::shared_ptr<spAnimationState>;
-        using skeleton_ptr = std::shared_ptr<spSkeleton>;
         using clipping_ptr = std::shared_ptr<spSkeletonClipping>;
-        using effect_ptr = std::shared_ptr<spVertexEffect>;
+        using skeleton_ptr = std::shared_ptr<spSkeleton>;
+        using animation_ptr = std::shared_ptr<spAnimationState>;
     public:
         spine_player() = default;
         spine_player(const spine_model_asset::ptr& model);
         
-        spine_player& model(const spine_model_asset::ptr& value) noexcept;
+        spine_player& model(const spine_model_asset::ptr& value);
+
         spine_player& skin(const str& value) noexcept;
         spine_player& attachment(const str& slot, const str& name) noexcept;
 
-        spine_player& time_scale(float value) noexcept;
-        [[nodiscard]] float time_scale() const noexcept;
+        bool has_skin(const str& name) const noexcept;
+        bool has_animation(const str& name) const noexcept;
 
-        [[nodiscard]] bool has_animation(const str& name) const noexcept;
-        
-        const animation_ptr& animation() const noexcept;
-        const skeleton_ptr& skeleton() const noexcept;
         const clipping_ptr& clipper() const noexcept;
+        const skeleton_ptr& skeleton() const noexcept;
+        const animation_ptr& animation() const noexcept;
         const spine_model_asset::ptr& model() const noexcept;
     private:
+        clipping_ptr clipping_;
+        skeleton_ptr skeleton_;
         animation_ptr animation_;
         spine_model_asset::ptr model_;
-        skeleton_ptr skeleton_;
-        clipping_ptr clipping_;
     };
 
     template <>
--- sources/enduro2d/high/components/spine_player.cpp
+++ sources/enduro2d/high/components/spine_player.cpp
@@ -5,6 +5,7 @@
  ******************************************************************************/
 
 #include <enduro2d/high/components/spine_player.hpp>
+
 #include <spine/spine.h>
 
 namespace e2d
@@ -13,67 +14,73 @@ namespace e2d
         this->model(model);
     }
     
-    spine_player& spine_player::model(const spine_model_asset::ptr& value) noexcept {
-        E2D_ASSERT(value);
-        E2D_ASSERT(value->content().skeleton());
-        E2D_ASSERT(value->content().atlas());
-        model_ = value;
-
-        skeleton_ = skeleton_ptr(spSkeleton_create(model_->content().skeleton().get()), spSkeleton_dispose);
-        clipping_ = clipping_ptr(spSkeletonClipping_create(), spSkeletonClipping_dispose);
-
-        if ( model_->content().animation() ) {
-            animation_ = animation_ptr(
-                spAnimationState_create(model_->content().animation().get()),
-                spAnimationState_dispose);
-            E2D_ASSERT(animation_); // TODO exception?
-        } else {
-            animation_.reset();
+    spine_player& spine_player::model(const spine_model_asset::ptr& value) {
+        clipping_ptr new_clipping = clipping_ptr(
+            spSkeletonClipping_create(),
+            spSkeletonClipping_dispose);
+        if ( !new_clipping ) {
+            throw std::bad_alloc();
         }
+
+        skeleton_ptr new_skeleton;
+        if ( value && value->content().skeleton() ) {
+            new_skeleton = skeleton_ptr(
+                spSkeleton_create(value->content().skeleton().get()),
+                spSkeleton_dispose);
+            if ( !new_skeleton ) {
+                throw std::bad_alloc();
+            }
+        }
+
+        animation_ptr new_animation;
+        if ( value && value->content().animation() ) {
+            new_animation = animation_ptr(
+                spAnimationState_create(value->content().animation().get()),
+                spAnimationState_dispose);
+            if ( !new_animation ) {
+                throw std::bad_alloc();
+            }
+        }
+
+        model_ = value;
+        clipping_ = std::move(new_clipping);
+        skeleton_ = std::move(new_skeleton);
+        animation_ = std::move(new_animation);
         return *this;
     }
 
     spine_player& spine_player::skin(const str& value) noexcept {
-        spSkeleton_setSkinByName(skeleton_.get(), value.empty() ? nullptr : value.c_str());
+        if ( !spSkeleton_setSkinByName(skeleton_.get(), value.empty() ? nullptr : value.c_str()) ) {
+            the<debug>().error("SPINE_PLAYER: can't set skin '%0'", value);
+        }
         return *this;
     }
     
     spine_player& spine_player::attachment(const str& slot, const str& name) noexcept {
-        E2D_ASSERT(!slot.empty());
-        E2D_ASSERT(!name.empty());
         if ( !spSkeleton_setAttachment(skeleton_.get(), slot.c_str(), name.c_str()) ) {
-            the<debug>().error("SPINE_RENDERER: can't set attachment '%0' to slot '%1'", name, slot);
+            the<debug>().error("SPINE_PLAYER: can't set attachment '%0' to slot '%1'", name, slot);
         }
         return *this;
     }
-    
-    const spine_player::skeleton_ptr& spine_player::skeleton() const noexcept {
-        return skeleton_;
+
+    bool spine_player::has_skin(const str& name) const noexcept {
+        return model_
+            && model()->content().skeleton()
+            && spSkeletonData_findSkin(model()->content().skeleton().get(), name.c_str());
     }
     
+    bool spine_player::has_animation(const str& name) const noexcept {
+        return model_
+            && model()->content().skeleton()
+            && spSkeletonData_findAnimation(model()->content().skeleton().get(), name.c_str());
+    }
+
     const spine_player::clipping_ptr& spine_player::clipper() const noexcept {
         return clipping_;
     }
 
-    spine_player& spine_player::time_scale(float value) noexcept {
-        E2D_ASSERT(animation_);
-        animation_->timeScale = value;
-        return *this;
-    }
-
-    float spine_player::time_scale() const noexcept {
-        E2D_ASSERT(animation_);
-        return animation_->timeScale;
-    }
-    
-    bool spine_player::has_animation(const str& name) const noexcept {
-        if ( !model_ ) {
-            return false;
-        }
-        spAnimation* anim = spSkeletonData_findAnimation(
-            model_->content().skeleton().get(),
-            name.c_str());
-        return anim != nullptr;
+    const spine_player::skeleton_ptr& spine_player::skeleton() const noexcept {
+        return skeleton_;
     }
 
     const spine_player::animation_ptr& spine_player::animation() const noexcept {
@@ -89,18 +96,19 @@ namespace e2d
 {
     const char* factory_loader<spine_player>::schema_source = R"json({
         "type" : "object",
-        "required" : [ "model" ],
+        "required" : [],
         "additionalProperties" : false,
         "properties" : {
             "model" : { "$ref": "#/common_definitions/address" },
             "skin" : { "$ref": "#/common_definitions/name" },
-            "attachments" : {
-                "type" : "array",
-                "items" : { "$ref": "#/definitions/spine_attachment" }
-            }
+            "attachments" : { "$ref": "#/definitions/attachments" }
         },
         "definitions" : {
-            "spine_attachment" : {
+            "attachments" : {
+                "type" : "array",
+                "items" : { "$ref": "#/definitions/attachment" }
+            },
+            "attachment" : {
                 "type" : "object",
                 "required" : [ "slot", "name" ],
                 "additionalProperties" : false,
@@ -127,16 +135,13 @@ namespace e2d
                     ctx.root["model"].GetString());
                 return false;
             }
-            component = spine_player(model);
-        } else {
-            the<debug>().error("SPINE_PLAYER: Property 'model' is not found");
-            return false;
+            component.model(model);
         }
         
         if ( ctx.root.HasMember("skin") ) {
             str skin;
             if ( !json_utils::try_parse_value(ctx.root["skin"], skin) ) {
-                the<debug>().error("SPINE RENDERER: Incorrect formatting of 'skin' property");
+                the<debug>().error("SPINE_PLAYER: Incorrect formatting of 'skin' property");
                 return false;
             }
             component.skin(skin);
@@ -148,18 +153,23 @@ namespace e2d
             
             for ( rapidjson::SizeType i = 0; i < attachments_json.Size(); ++i ) {
                 E2D_ASSERT(attachments_json[i].IsObject());
-                const auto& item_json = attachments_json[i];
+                const auto& attachment_json = attachments_json[i];
 
-                if ( !item_json.HasMember("slot") ) {
-                    the<debug>().error("SPINE_PLAYER: Incorrect formatting of 'attachments.slot' property");
-                    return false;
-                }
-                if ( !item_json.HasMember("name") ) {
-                    the<debug>().error("SPINE_PLAYER: Incorrect formatting of 'attachments.name' property");
+                str slot;
+                E2D_ASSERT(attachment_json.HasMember("slot"));
+                if ( !json_utils::try_parse_value(attachment_json["slot"], slot) ) {
+                    the<debug>().error("SPINE_PLAYER: Incorrect formatting of 'attachment.slot' property");
                     return false;
                 }
 
-                component.attachment(item_json["slot"].GetString(), item_json["name"].GetString());
+                str name;
+                E2D_ASSERT(attachment_json.HasMember("name"));
+                if ( !json_utils::try_parse_value(attachment_json["name"], name) ) {
+                    the<debug>().error("SPINE_PLAYER: Incorrect formatting of 'attachment.name' property");
+                    return false;
+                }
+
+                component.attachment(slot, name);
             }
         }
 
@@ -174,6 +184,7 @@ namespace e2d
             dependencies.add_dependency<spine_model_asset>(
                 path::combine(ctx.parent_address, ctx.root["model"].GetString()));
         }
+
         return true;
     }
 }
