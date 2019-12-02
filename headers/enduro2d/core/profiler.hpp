dbf86793f0da592865b6920b298b495a1f80ce06
BlackMATov <blackmatov@gmail.com>
2019-12-02 11:12:01 +0700

    basic profiler events and sink registration

--- headers/enduro2d/core/profiler.hpp
+++ headers/enduro2d/core/profiler.hpp
@@ -12,10 +12,77 @@ namespace e2d
 {
     class profiler final : public module<profiler> {
     public:
+        struct begin_scope_info {
+            str name;
+            u64 ts = 0;
+            u64 tid = 0;
+        };
+
+        struct end_scope_info {
+            u64 ts = 0;
+            u64 tid = 0;
+        };
+
+        struct thread_event_info {
+            str name;
+            u64 ts = 0;
+            u64 tid = 0;
+        };
+
+        struct global_event_info {
+            str name;
+            u64 ts = 0;
+            u64 tid = 0;
+        };
+
+        using event_info = std::variant<
+            begin_scope_info,
+            end_scope_info,
+            thread_event_info,
+            global_event_info>;
+    public:
+        class sink : private e2d::noncopyable {
+        public:
+            virtual ~sink() noexcept = default;
+            virtual void on_event(const event_info& event) noexcept = 0;
+        };
+        using sink_uptr = std::unique_ptr<sink>;
+    public:
+        class scope final : private e2d::noncopyable {
+        public:
+            scope(profiler& profiler, str name);
+            ~scope() noexcept;
+        private:
+            profiler& profiler_;
+        };
+    public:
         profiler();
         ~profiler() noexcept final;
+
+        void begin_scope(str name) noexcept;
+        void end_scope() noexcept;
+
+        void thread_event(str name) noexcept;
+        void global_event(str name) noexcept;
+    public:
+        template < typename T, typename... Args >
+        T& register_sink(Args&&... args);
+        sink& register_sink(sink_uptr sink);
+        void unregister_sink(const sink& sink) noexcept;
     private:
         class internal_state;
         std::unique_ptr<internal_state> state_;
     };
+}
+
+#define E2D_PROFILER_SCOPE(name)\
+    auto E2D_PP_CAT(e2d_generated_profiler_scope_, __LINE__) = ::e2d::profiler::scope(the<profiler>(), name)
+
+namespace e2d
+{
+    template < typename T, typename... Args >
+    T& profiler::register_sink(Args&&... args) {
+        return static_cast<T&>(
+            register_sink(std::make_unique<T>(std::forward<Args>(args)...)));
+    }
 }
--- sources/enduro2d/core/profiler.cpp
+++ sources/enduro2d/core/profiler.cpp
@@ -8,13 +8,93 @@
 
 namespace e2d
 {
+    profiler::scope::scope(profiler& profiler, str name)
+    : profiler_(profiler) {
+        profiler_.begin_scope(std::move(name));
+    }
+
+    profiler::scope::~scope() noexcept {
+        profiler_.end_scope();
+    }
+}
+
+namespace e2d
+{
     class profiler::internal_state final : private e2d::noncopyable {
     public:
         internal_state() = default;
         ~internal_state() noexcept = default;
-    };
 
+        sink& register_sink(sink_uptr sink) {
+            E2D_ASSERT(sink);
+            std::lock_guard<std::recursive_mutex> guard(rmutex_);
+            sinks_.push_back(std::move(sink));
+            return *sinks_.back();
+        }
+
+        void unregister_sink(const sink& sink) noexcept {
+            std::lock_guard<std::recursive_mutex> guard(rmutex_);
+            for ( auto iter = sinks_.begin(); iter != sinks_.end(); ) {
+                if ( iter->get() == &sink ) {
+                    iter = sinks_.erase(iter);
+                } else {
+                    ++iter;
+                }
+            }
+        }
+
+        void emit_event(event_info event) noexcept {
+            std::lock_guard<std::recursive_mutex> guard(rmutex_);
+            for ( const auto& sink : sinks_ ) {
+                if ( sink ) {
+                    sink->on_event(event);
+                }
+            }
+        }
+    private:
+        vector<sink_uptr> sinks_;
+        std::recursive_mutex rmutex_;
+    };
+}
+
+namespace e2d
+{
     profiler::profiler()
     : state_(new internal_state()) {}
     profiler::~profiler() noexcept = default;
+
+    profiler::sink& profiler::register_sink(sink_uptr sink) {
+        return state_->register_sink(std::move(sink));
+    }
+
+    void profiler::unregister_sink(const sink& sink) noexcept {
+        state_->unregister_sink(sink);
+    }
+
+    void profiler::begin_scope(str name) noexcept {
+        state_->emit_event(begin_scope_info{
+            std::move(name),
+            math::numeric_cast<u64>(time::now_us().value),
+            std::hash<std::thread::id>()(std::this_thread::get_id())});
+    }
+
+    void profiler::end_scope() noexcept {
+        state_->emit_event(end_scope_info{
+            math::numeric_cast<u64>(time::now_us().value),
+            std::hash<std::thread::id>()(std::this_thread::get_id())});
+    }
+
+    void profiler::thread_event(str name) noexcept {
+        state_->emit_event(thread_event_info{
+            std::move(name),
+            math::numeric_cast<u64>(time::now_us().value),
+            std::hash<std::thread::id>()(std::this_thread::get_id())});
+    }
+
+    void profiler::global_event(str name) noexcept {
+        state_->emit_event(global_event_info{
+            std::move(name),
+            math::numeric_cast<u64>(time::now_us().value),
+            std::hash<std::thread::id>()(std::this_thread::get_id())});
+    }
 }
