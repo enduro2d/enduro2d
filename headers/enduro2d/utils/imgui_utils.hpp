6371effc33ac79cd1641ffeeed2e4e6052243efe
BlackMATov <blackmatov@gmail.com>
2019-12-12 00:43:41 +0700

    dummy imgui utils

---
+++ headers/enduro2d/utils/imgui_utils.hpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * This file is part of the "Enduro2D"
+ * For conditions of distribution and use, see copyright notice in LICENSE.md
+ * Copyright (C) 2018-2019, by Matvey Cherevko (blackmatov@gmail.com)
+ ******************************************************************************/
+
+#pragma once
+
+#include "_utils.hpp"
+
+#include <3rdparty/imgui/imgui.h>
+#include <3rdparty/imgui/imgui_stdlib.h>
+#include <3rdparty/imgui/imgui_internal.h>
+
+namespace e2d::imgui_utils
+{
+}
