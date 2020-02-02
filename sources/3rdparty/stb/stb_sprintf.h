1e28f05c0790340325cae553316b784284c28deb
BlackMATov <blackmatov@gmail.com>
2020-02-03 05:54:09 +0700

    update stb module

--- sources/3rdparty/stb/stb_sprintf.h
+++ sources/3rdparty/stb/stb_sprintf.h
@@ -1,9 +1,9 @@
-// stb_sprintf - v1.06 - public domain snprintf() implementation
+// stb_sprintf - v1.07 - public domain snprintf() implementation
 // originally by Jeff Roberts / RAD Game Tools, 2015/10/20
 // http://github.com/nothings/stb
 //
 // allowed types:  sc uidBboXx p AaGgEef n
-// lengths      :  h ll j z t I64 I32 I
+// lengths      :  hh h ll j z t I64 I32 I
 //
 // Contributors:
 //    Fabian "ryg" Giesen (reformatting)
@@ -18,6 +18,8 @@
 //    Leonard Ritter
 //    Stefano Zanotti
 //    Adam Allison
+//    Arvid Gerstmann
+//    Markus Kolb
 //
 // LICENSE:
 //
@@ -139,25 +141,38 @@ PERFORMANCE vs MSVC 2008 32-/64-bit (GCC is even slower than MSVC):
 "...512 char string..." ( 35.0x/32.5x faster!)
 */
 
-#if defined(__has_feature)
-   #if __has_feature(address_sanitizer)
-      #define STBI__ASAN __attribute__((no_sanitize("address")))
+#if defined(__clang__)
+ #if defined(__has_feature) && defined(__has_attribute)
+  #if __has_feature(address_sanitizer)
+   #if __has_attribute(__no_sanitize__)
+    #define STBSP__ASAN __attribute__((__no_sanitize__("address")))
+   #elif __has_attribute(__no_sanitize_address__)
+    #define STBSP__ASAN __attribute__((__no_sanitize_address__))
+   #elif __has_attribute(__no_address_safety_analysis__)
+    #define STBSP__ASAN __attribute__((__no_address_safety_analysis__))
    #endif
+  #endif
+ #endif
+#elif __GNUC__ >= 5 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8)
+ #if __SANITIZE_ADDRESS__
+  #define STBSP__ASAN __attribute__((__no_sanitize_address__))
+ #endif
 #endif
-#ifndef STBI__ASAN
-#define STBI__ASAN
+
+#ifndef STBSP__ASAN
+#define STBSP__ASAN
 #endif
 
 #ifdef STB_SPRINTF_STATIC
 #define STBSP__PUBLICDEC static
-#define STBSP__PUBLICDEF static STBI__ASAN
+#define STBSP__PUBLICDEF static STBSP__ASAN
 #else
 #ifdef __cplusplus
 #define STBSP__PUBLICDEC extern "C"
-#define STBSP__PUBLICDEF extern "C" STBI__ASAN
+#define STBSP__PUBLICDEF extern "C" STBSP__ASAN
 #else
 #define STBSP__PUBLICDEC extern
-#define STBSP__PUBLICDEF STBI__ASAN
+#define STBSP__PUBLICDEF STBSP__ASAN
 #endif
 #endif
 
@@ -200,7 +215,7 @@ STBSP__PUBLICDEF void STB_SPRINTF_DECORATE(set_separators)(char comma, char peri
 #define stbsp__uint16 unsigned short
 
 #ifndef stbsp__uintptr
-#if defined(__ppc64__) || defined(__aarch64__) || defined(_M_X64) || defined(__x86_64__) || defined(__x86_64)
+#if defined(__ppc64__) || defined(__powerpc64__) || defined(__aarch64__) || defined(_M_X64) || defined(__x86_64__) || defined(__x86_64)
 #define stbsp__uintptr stbsp__uint64
 #else
 #define stbsp__uintptr stbsp__uint32
@@ -457,6 +472,8 @@ STBSP__PUBLICDEF int STB_SPRINTF_DECORATE(vsprintfcb)(STBSP_SPRINTFCB *callback,
       case 'h':
          fl |= STBSP__HALFWIDTH;
          ++f;
+         if (f[0] == 'h')
+            ++f;  // QUARTERWIDTH
          break;
       // are we 64-bit (unix style)
       case 'l':
@@ -1342,12 +1359,14 @@ STBSP__PUBLICDEF int STB_SPRINTF_DECORATE(sprintf)(char *buf, char const *fmt, .
 typedef struct stbsp__context {
    char *buf;
    int count;
+   int length;
    char tmp[STB_SPRINTF_MIN];
 } stbsp__context;
 
 static char *stbsp__clamp_callback(char *buf, void *user, int len)
 {
    stbsp__context *c = (stbsp__context *)user;
+   c->length += len;
 
    if (len > c->count)
       len = c->count;
@@ -1367,7 +1386,7 @@ static char *stbsp__clamp_callback(char *buf, void *user, int len)
    }
 
    if (c->count <= 0)
-      return 0;
+      return c->tmp;
    return (c->count >= STB_SPRINTF_MIN) ? c->buf : c->tmp; // go direct into buffer if you can
 }
 
@@ -1375,29 +1394,27 @@ static char * stbsp__count_clamp_callback( char * buf, void * user, int len )
 {
    stbsp__context * c = (stbsp__context*)user;
 
-   c->count += len;
+   c->length += len;
    return c->tmp; // go direct into buffer if you can
 }
 
 STBSP__PUBLICDEF int STB_SPRINTF_DECORATE( vsnprintf )( char * buf, int count, char const * fmt, va_list va )
 {
    stbsp__context c;
-   int l;
 
    if ( (count == 0) && !buf )
    {
-      c.count = 0;
+      c.length = 0;
 
       STB_SPRINTF_DECORATE( vsprintfcb )( stbsp__count_clamp_callback, &c, c.tmp, fmt, va );
-      l = c.count;
    }
    else
    {
-      if ( count == 0 )
-         return 0;
+      int l;
 
       c.buf = buf;
       c.count = count;
+      c.length = 0;
 
       STB_SPRINTF_DECORATE( vsprintfcb )( stbsp__clamp_callback, &c, stbsp__clamp_callback(0,&c,0), fmt, va );
 
@@ -1408,7 +1425,7 @@ STBSP__PUBLICDEF int STB_SPRINTF_DECORATE( vsnprintf )( char * buf, int count, c
       buf[l] = 0;
    }
 
-   return l;
+   return c.length;
 }
 
 STBSP__PUBLICDEF int STB_SPRINTF_DECORATE(snprintf)(char *buf, int count, char const *fmt, ...)
