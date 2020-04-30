b8eeeaca703f03dcaffe2de8644f0d3a7670830a
BlackMATov <blackmat@mail.ru>
2020-04-30 07:50:35 +0700

    hide pugixml sources

--- scripts/update_modules.sh
+++ scripts/update_modules.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ROOT_DIR=`dirname "$BASH_SOURCE"`/..
+
 MODULES_DIR=$ROOT_DIR/modules
 HEADERS_DIR=$ROOT_DIR/headers
 SOURCES_DIR=$ROOT_DIR/sources
 UNTESTS_DIR=$ROOT_DIR/untests
+
 HEADERS_RDPARTY_DIR=$HEADERS_DIR/3rdparty
 SOURCES_RDPARTY_DIR=$SOURCES_DIR/3rdparty
 
@@ -14,8 +16,16 @@ mkdir -p $UNTESTS_DIR
 mkdir -p $HEADERS_RDPARTY_DIR
 mkdir -p $SOURCES_RDPARTY_DIR
 
+#
+# catch
+#
+
 mkdir -p $UNTESTS_DIR/catch
 cp -fv $MODULES_DIR/catch2/single_include/catch2/catch.hpp $UNTESTS_DIR/catch/catch.hpp
+
+#
+# imgui
+#
 
 mkdir -p $HEADERS_RDPARTY_DIR/imgui
 cp -fv $MODULES_DIR/imgui/imgui_internal.h $HEADERS_RDPARTY_DIR/imgui/imgui_internal.h
@@ -31,6 +41,10 @@ cp -fv $MODULES_DIR/imgui/imgui_demo.cpp $SOURCES_RDPARTY_DIR/imgui/imgui_demo.c
 cp -fv $MODULES_DIR/imgui/imgui_draw.cpp $SOURCES_RDPARTY_DIR/imgui/imgui_draw.cpp
 cp -fv $MODULES_DIR/imgui/imgui_widgets.cpp $SOURCES_RDPARTY_DIR/imgui/imgui_widgets.cpp
 cp -fv $MODULES_DIR/imgui/misc/cpp/imgui_stdlib.cpp $SOURCES_RDPARTY_DIR/imgui/imgui_stdlib.cpp
+
+#
+# lua
+#
 
 mkdir -p $HEADERS_RDPARTY_DIR/lua
 cp -fv $MODULES_DIR/lua/lauxlib.h $HEADERS_RDPARTY_DIR/lua/lauxlib.h
@@ -94,6 +108,10 @@ cp -fv $MODULES_DIR/lua/lvm.h $SOURCES_RDPARTY_DIR/lua/lvm.h
 cp -fv $MODULES_DIR/lua/lzio.c $SOURCES_RDPARTY_DIR/lua/lzio.c
 cp -fv $MODULES_DIR/lua/lzio.h $SOURCES_RDPARTY_DIR/lua/lzio.h
 
+#
+# miniz
+#
+
 mkdir -p $SOURCES_RDPARTY_DIR/miniz
 cp -fv $MODULES_DIR/miniz/miniz.c $SOURCES_RDPARTY_DIR/miniz/miniz.c
 cp -fv $MODULES_DIR/miniz/miniz.h $SOURCES_RDPARTY_DIR/miniz/miniz.h
@@ -105,17 +123,41 @@ cp -fv $MODULES_DIR/miniz/miniz_tinfl.h $SOURCES_RDPARTY_DIR/miniz/miniz_tinfl.h
 cp -fv $MODULES_DIR/miniz/miniz_zip.c $SOURCES_RDPARTY_DIR/miniz/miniz_zip.c
 cp -fv $MODULES_DIR/miniz/miniz_zip.h $SOURCES_RDPARTY_DIR/miniz/miniz_zip.h
 
+#
+# pnpoly
+#
+
 mkdir -p $SOURCES_RDPARTY_DIR/pnpoly.h
 cp -fv $MODULES_DIR/pnpoly.h/pnpoly.h $SOURCES_RDPARTY_DIR/pnpoly.h/pnpoly.h
 
+#
+# pugixml
+#
+
 mkdir -p $HEADERS_RDPARTY_DIR/pugixml
-cp -rfv $MODULES_DIR/pugixml/src/. $HEADERS_RDPARTY_DIR/pugixml/
+cp -fv $MODULES_DIR/pugixml/src/pugiconfig.hpp $HEADERS_RDPARTY_DIR/pugixml/pugiconfig.hpp
+cp -fv $MODULES_DIR/pugixml/src/pugixml.hpp $HEADERS_RDPARTY_DIR/pugixml/pugixml.hpp
+
+mkdir -p $SOURCES_RDPARTY_DIR/pugixml
+cp -fv $MODULES_DIR/pugixml/src/pugixml.cpp $SOURCES_RDPARTY_DIR/pugixml/pugixml.cpp
+
+#
+# rapidjson
+#
 
 mkdir -p $HEADERS_RDPARTY_DIR/rapidjson
 cp -rfv $MODULES_DIR/rapidjson/include/rapidjson/. $HEADERS_RDPARTY_DIR/rapidjson
 
+#
+# sol
+#
+
 mkdir -p $HEADERS_RDPARTY_DIR/sol
 cp -rfv $MODULES_DIR/sol2/include/sol/. $HEADERS_RDPARTY_DIR/sol
+
+#
+# stb
+#
 
 mkdir -p $SOURCES_RDPARTY_DIR/stb
 cp -fv $MODULES_DIR/stb/stb_image.h $SOURCES_RDPARTY_DIR/stb/stb_image.h
@@ -125,8 +167,16 @@ cp -fv $MODULES_DIR/stb/stb_rect_pack.h $SOURCES_RDPARTY_DIR/stb/stb_rect_pack.h
 cp -fv $MODULES_DIR/stb/stb_sprintf.h $SOURCES_RDPARTY_DIR/stb/stb_sprintf.h
 cp -fv $MODULES_DIR/stb/stb_truetype.h $SOURCES_RDPARTY_DIR/stb/stb_truetype.h
 
+#
+# utfcpp
+#
+
 mkdir -p $SOURCES_RDPARTY_DIR/utfcpp
 cp -rfv $MODULES_DIR/utfcpp/source/. $SOURCES_RDPARTY_DIR/utfcpp/
+
+#
+# yoga
+#
 
 mkdir -p $SOURCES_RDPARTY_DIR/yoga
 cp -rfv $MODULES_DIR/yoga/yoga/. $SOURCES_RDPARTY_DIR/yoga/
